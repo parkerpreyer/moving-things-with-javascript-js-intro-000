f1220139fccc4fa28a1ac8ab05788126a31fc850
Parker Preyer <parker.preyer@gmail.com>
2019-11-14 00:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const app = "I don't do much."
 
 const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = '0px';
 
-dodger.style.backgroundColor = "#000000";
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right - 10}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+  if (e.which === 39) {
+    moveDodgerRight();
+})
