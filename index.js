92d2304464573348af312fd1520cf5a07e3d85bc
Parker Preyer <parker.preyer@gmail.com>
2019-11-14 00:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function moveDodgerRight() {
   var right = parseInt(rightNumbers, 10);
 
   if (right > 0) {
-    dodger.style.right = `${right - 10}px`
+    dodger.style.right = `${right - 10}px`;
   }
 }
 
